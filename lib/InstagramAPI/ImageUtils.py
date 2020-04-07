4959cdacfe78a63c4a5a9f03ec31443dc34b5085
MrTuxx <mrtuxgreyhat@protonmail.com>
2020-04-08 00:28:03 +0200

    Instagram API

---
+++ lib/InstagramAPI/ImageUtils.py
@@ -0,0 +1,33 @@
+import struct
+import imghdr
+
+
+def getImageSize(fname):
+    with open(fname, 'rb') as fhandle:
+        head = fhandle.read(24)
+        if len(head) != 24:
+            raise RuntimeError("Invalid Header")
+        if imghdr.what(fname) == 'png':
+            check = struct.unpack('>i', head[4:8])[0]
+            if check != 0x0d0a1a0a:
+                raise RuntimeError("PNG: Invalid check")
+            width, height = struct.unpack('>ii', head[16:24])
+        elif imghdr.what(fname) == 'gif':
+            width, height = struct.unpack('<HH', head[6:10])
+        elif imghdr.what(fname) == 'jpeg':
+            fhandle.seek(0)  # Read 0xff next
+            size = 2
+            ftype = 0
+            while not 0xc0 <= ftype <= 0xcf:
+                fhandle.seek(size, 1)
+                byte = fhandle.read(1)
+                while ord(byte) == 0xff:
+                    byte = fhandle.read(1)
+                ftype = ord(byte)
+                size = struct.unpack('>H', fhandle.read(2))[0] - 2
+            # We are at a SOFn block
+            fhandle.seek(1, 1)  # Skip `precision' byte.
+            height, width = struct.unpack('>HH', fhandle.read(4))
+        else:
+            raise RuntimeError("Unsupported format")
+        return width, height
---
+++ lib/InstagramAPI/__init__.py
@@ -0,0 +1,2 @@
+from .InstagramAPI import *
+from .ImageUtils import *
